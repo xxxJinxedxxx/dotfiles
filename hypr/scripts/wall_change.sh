25ac6eb51bcf544c130bc64c4a6f81cc70c8fa90
TODARO Gaetan <todaro.gaetan@gmail.com>
2025-11-09 13:29:36 +0100

    update

--- hypr/scripts/wall_change.sh
+++ hypr/scripts/wall_change.sh
@@ -6,6 +6,7 @@ WALLPAPER=`ls $D | shuf | head -n 1`
 swww img "$D/$WALLPAPER" --transition-type center --transition-step 100 --transition-fps 60
 
 wal -n -i $D/$WALLPAPER
+themecord -p
 
 # exec "~/.config/waybar/launch.sh"
 
